64a88d4eed76285df6c293dad352fd095f122949
HaKieuTrang <hakieu.trang55@gmail.com>
2020-04-17 10:02:33 +0700

    select data

--- select_data.py
+++ select_data.py
@@ -1,13 +1,23 @@
 import mysql.connector
 
+connection = mysql.connector.connect(host='localhost',
+                                     database='clamav_scan',
+                                     user='admin',
+                                     password='Trang1234#')
 
-def select_data():
-    connection = mysql.connector.connect(host='localhost',
-                                         database='clamav_scan',
-                                         user='admin',
-                                         password='Trang1234#')
 
+def select_data_scan():
     mysql_select_query = """SELECT * FROM scan_info"""
+    cursor = connection.cursor()
+    cursor.execute(mysql_select_query)
+    data = cursor.fetchall()
+    connection.commit()
+    cursor.close()
+    return data
+
+
+def select_data_signatures():
+    mysql_select_query = """SELECT * FROM clamscan_signatures"""
     cursor = connection.cursor()
     cursor.execute(mysql_select_query)
     data = cursor.fetchall()
